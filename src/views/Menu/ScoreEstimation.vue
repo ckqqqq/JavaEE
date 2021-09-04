5ec3da7e059bdbd9902cd283424dd0e9e582533f
ckqqqq <1047969038@qq.com>
2021-09-04 17:38:15 +0800

    完善了管理数据库界面

--- src/views/Menu/ScoreEstimation.vue
+++ src/views/Menu/ScoreEstimation.vue
@@ -2,6 +2,34 @@
     <div class="about">
         <h1>This is an ScoreEstimate page 23333</h1>
         <h1>估分</h1>
+        <v-container>
+            <v-card outlined class="text-xs-center ma-3">
+                <v-card-title>标题</v-card-title>
+                <v-card-text>
+                    <div class="subheading">Mike</div>
+                    <div class="grey--text">前端工程师</div>
+                </v-card-text>
+                <v-card-actions>
+                    <v-btn outlined color="grey">
+                        <v-icon small left>message</v-icon>
+                        <span>发送消息</span>
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+            <v-card outlined class="text-xs-center ma-3">
+                <v-card-title>标题</v-card-title>
+                <v-card-text>
+                    <div class="subheading">Mike</div>
+                    <div class="grey--text">前端工程师</div>
+                </v-card-text>
+                <v-card-actions>
+                    <v-btn outlined color="grey">
+                        <v-icon small left>message</v-icon>
+                        <span>发送消息</span>
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-container>
 
     </div>
 </template>
