6608f00707fa871de5af847a5877abec8376eabc
ckqqqq <1047969038@qq.com>
2021-09-05 12:19:49 +0800

    杨兄的代码

--- src/views/Menu/SchoolInformation.vue
+++ src/views/Menu/SchoolInformation.vue
@@ -1,132 +1,185 @@
-<template>
-    <v-container>
-        <v-layout text-center wrap>
-            <v-flex xs12>
-                <v-img
-                        :src="require('../../assets/logo.svg')"
-                        class="my-3"
-                        contain
-                        height="200"
-                ></v-img>
-            </v-flex>
 
-            <v-flex mb-4>
-                <h1 class="display-2 font-weight-bold mb-3">
-                    Welcome to Vuetify
-                </h1>
-                <p class="subheading font-weight-regular">
-                    For help and collaboration with other Vuetify developers,
-                    <br />please join our online
-                    <a href="https://community.vuetifyjs.com" target="_blank"
-                    >Discord Community</a
-                    >
+
+
+
+<template xmlns:white-space="http://www.w3.org/1999/xhtml">
+
+    <v-card
+            class="mx-auto"
+            max-width="3000"
+            max-height="3000"
+            tile
+    >
+        <v-container>
+            <div   class="font-weight-medium"
+
+            >
+                北京邮电大学
+            </div>
+        </v-container>
+        <v-list-item>
+            <v-list-item-content>
+                <v-expand-transition>
+                    <v-avatar max-width="300" max-height="3000" height="100" width="100">
+                        <img src="https://picsum.photos/id/11/500/300">
+                        <div id="bupt">
+                            <div   class="font-weight-medium">
+                                北京邮电大学
+                            </div>              &nbsp;&nbsp;&nbsp;Beijing University of Posts and Telecommunications
+                        </div>
+                    </v-avatar>
+                </v-expand-transition>
+
+                <p class="text-justify">
+                    <a href="https://community.vuetifyjs.com" target="_blank"-->
+                                            >Discord Community</a>
+                    <br>
+                    北京邮电大学（Beijing University of Posts and Telecommunications），简称北邮，是中华人民共和国教育部直属、
+                    工业和信息化部共建的全国重点大学，位列国家“世界一流学科建设高校”、“211工程”、“985工程优势学科创新平台”，为北京高科大学联
+                    盟、中国人工智能教育联席会、CDIO工程教育联盟创始成员，入选国家“111计划”、“2011计划”、中国政府奖学金来华留学生接收院校、
+                    教育部第一批卓越工程师教育培养计划高校、国家建设高水平大学公派研究生项目、教育部首批“新工科”研究与实践项目、全国首批深化创
+                    新创业教育改革示范高校、一流网络安全学院建设示范项目高校、国家大学生文化素质教育基地、全国邮政行业人才培养基地。
                 </p>
-            </v-flex>
 
-            <v-flex mb-5 xs12>
-                <h2 class="headline font-weight-bold mb-3">What's next?</h2>
+            </v-list-item-content>
 
-                <v-layout justify-center>
-                    <a
-                            v-for="(next, i) in whatsNext"
-                            :key="i"
-                            :href="next.href"
-                            class="subheading mx-3"
-                            target="_blank"
-                    >
-                        {{ next.text }}
-                    </a>
-                </v-layout>
-            </v-flex>
+        </v-list-item>
 
-            <v-flex xs12 mb-5>
-                <h2 class="headline font-weight-bold mb-3">Important Links</h2>
 
-                <v-layout justify-center>
-                    <a
-                            v-for="(link, i) in importantLinks"
-                            :key="i"
-                            :href="link.href"
-                            class="subheading mx-3"
-                            target="_blank"
-                    >
-                        {{ link.text }}
-                    </a>
-                </v-layout>
-            </v-flex>
+    </v-card>
 
-            <v-flex xs12 mb-5>
-                <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
-                <v-layout justify-center>
-                    <a
-                            v-for="(eco, i) in ecosystem"
-                            :key="i"
-                            :href="eco.href"
-                            class="subheading mx-3"
-                            target="_blank"
-                    >
-                        {{ eco.text }}
-                    </a>
-                </v-layout>
-            </v-flex>
-        </v-layout>
-    </v-container>
 </template>
 
-<script>
-    export default {
-        data: () => ({
-            ecosystem: [
-                {
-                    text: 'vuetify-loader',
-                    href: 'https://github.com/vuetifyjs/vuetify-loader'
-                },
-                {
-                    text: 'github',
-                    href: 'https://github.com/vuetifyjs/vuetify'
-                },
-                {
-                    text: 'awesome-vuetify',
-                    href: 'https://github.com/vuetifyjs/awesome-vuetify'
-                }
-            ],
-            importantLinks: [
-                {
-                    text: 'Documentation',
-                    href: 'https://vuetifyjs.com'
-                },
-                {
-                    text: 'Chat',
-                    href: 'https://community.vuetifyjs.com'
-                },
-                {
-                    text: 'Made with Vuetify',
-                    href: 'https://madewithvuejs.com/vuetify'
-                },
-                {
-                    text: 'Twitter',
-                    href: 'https://twitter.com/vuetifyjs'
-                },
-                {
-                    text: 'Articles',
-                    href: 'https://medium.com/vuetify'
-                }
-            ],
-            whatsNext: [
-                {
-                    text: 'Explore components',
-                    href: 'https://vuetifyjs.com/components/api-explorer'
-                },
-                {
-                    text: 'Select a layout',
-                    href: 'https://vuetifyjs.com/layout/pre-defined'
-                },
-                {
-                    text: 'Frequently Asked Questions',
-                    href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
-                }
-            ]
-        })
-    }
-</script>
+
+<!--<template>-->
+<!--    <v-container>-->
+<!--        <v-layout text-center wrap>-->
+<!--            <v-flex xs12>-->
+<!--                <v-img-->
+<!--                        :src="require('../../assets/logo.svg')"-->
+<!--                        class="my-3"-->
+<!--                        contain-->
+<!--                        height="200"-->
+<!--                ></v-img>-->
+<!--            </v-flex>-->
+
+<!--            <v-flex mb-4>-->
+<!--                <h1 class="display-2 font-weight-bold mb-3">-->
+<!--                    Welcome to Vuetify-->
+<!--                </h1>-->
+<!--                <p class="subheading font-weight-regular">-->
+<!--                    For help and collaboration with other Vuetify developers,-->
+<!--                    <br />please join our online-->
+<!--                    <a href="https://community.vuetifyjs.com" target="_blank"-->
+<!--                    >Discord Community</a-->
+<!--                    >-->
+<!--                </p>-->
+<!--            </v-flex>-->
+
+<!--            <v-flex mb-5 xs12>-->
+<!--                <h2 class="headline font-weight-bold mb-3">What's next?</h2>-->
+
+<!--                <v-layout justify-center>-->
+<!--                    <a-->
+<!--                            v-for="(next, i) in whatsNext"-->
+<!--                            :key="i"-->
+<!--                            :href="next.href"-->
+<!--                            class="subheading mx-3"-->
+<!--                            target="_blank"-->
+<!--                    >-->
+<!--                        {{ next.text }}-->
+<!--                    </a>-->
+<!--                </v-layout>-->
+<!--            </v-flex>-->
+
+<!--            <v-flex xs12 mb-5>-->
+<!--                <h2 class="headline font-weight-bold mb-3">Important Links</h2>-->
+
+<!--                <v-layout justify-center>-->
+<!--                    <a-->
+<!--                            v-for="(link, i) in importantLinks"-->
+<!--                            :key="i"-->
+<!--                            :href="link.href"-->
+<!--                            class="subheading mx-3"-->
+<!--                            target="_blank"-->
+<!--                    >-->
+<!--                        {{ link.text }}-->
+<!--                    </a>-->
+<!--                </v-layout>-->
+<!--            </v-flex>-->
+
+<!--            <v-flex xs12 mb-5>-->
+<!--                <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>-->
+
+<!--                <v-layout justify-center>-->
+<!--                    <a-->
+<!--                            v-for="(eco, i) in ecosystem"-->
+<!--                            :key="i"-->
+<!--                            :href="eco.href"-->
+<!--                            class="subheading mx-3"-->
+<!--                            target="_blank"-->
+<!--                    >-->
+<!--                        {{ eco.text }}-->
+<!--                    </a>-->
+<!--                </v-layout>-->
+<!--            </v-flex>-->
+<!--        </v-layout>-->
+<!--    </v-container>-->
+<!--</template>-->
+
+<!--<script>-->
+<!--    export default {-->
+<!--        data: () => ({-->
+<!--            ecosystem: [-->
+<!--                {-->
+<!--                    text: 'vuetify-loader',-->
+<!--                    href: 'https://github.com/vuetifyjs/vuetify-loader'-->
+<!--                },-->
+<!--                {-->
+<!--                    text: 'github',-->
+<!--                    href: 'https://github.com/vuetifyjs/vuetify'-->
+<!--                },-->
+<!--                {-->
+<!--                    text: 'awesome-vuetify',-->
+<!--                    href: 'https://github.com/vuetifyjs/awesome-vuetify'-->
+<!--                }-->
+<!--            ],-->
+<!--            importantLinks: [-->
+<!--                {-->
+<!--                    text: 'Documentation',-->
+<!--                    href: 'https://vuetifyjs.com'-->
+<!--                },-->
+<!--                {-->
+<!--                    text: 'Chat',-->
+<!--                    href: 'https://community.vuetifyjs.com'-->
+<!--                },-->
+<!--                {-->
+<!--                    text: 'Made with Vuetify',-->
+<!--                    href: 'https://madewithvuejs.com/vuetify'-->
+<!--                },-->
+<!--                {-->
+<!--                    text: 'Twitter',-->
+<!--                    href: 'https://twitter.com/vuetifyjs'-->
+<!--                },-->
+<!--                {-->
+<!--                    text: 'Articles',-->
+<!--                    href: 'https://medium.com/vuetify'-->
+<!--                }-->
+<!--            ],-->
+<!--            whatsNext: [-->
+<!--                {-->
+<!--                    text: 'Explore components',-->
+<!--                    href: 'https://vuetifyjs.com/components/api-explorer'-->
+<!--                },-->
+<!--                {-->
+<!--                    text: 'Select a layout',-->
+<!--                    href: 'https://vuetifyjs.com/layout/pre-defined'-->
+<!--                },-->
+<!--                {-->
+<!--                    text: 'Frequently Asked Questions',-->
+<!--                    href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'-->
+<!--                }-->
+<!--            ]-->
+<!--        })-->
+<!--    }-->
+<!--</script>-->
--- src/views/Menu/ScoreEstimation.vue
+++ src/views/Menu/ScoreEstimation.vue
@@ -2,15 +2,50 @@
     <div class="about">
         <h1>This is an ScoreEstimate page 23333</h1>
         <h1>估分</h1>
+        <v-container class="grey lighten-5">
+            <v-row no-gutters>
+                <template v-for="n in 4">
+                    <v-col :key="n">
+                        <v-card
+                                class="pa-2"
+                                outlined
+                                tile
+                        >
+                            <v-card outlined class="text-xs-center ma-3"
+                                    outlined color="green "
+                            >
+                                <v-card-title>语文</v-card-title>
+                                <!--                <v-card-title>标题</v-card-title>-->
+                                <v-card-text>
+                                    <div class="subheading">点击查看答案</div>
+                                </v-card-text>
+                                <v-card-actions>
+                                    <v-btn outlined color="green ">
+                                        <v-icon small left>message</v-icon>
+                                        <span>查看答案</span>
+                                    </v-btn>
+                                </v-card-actions>
+                            </v-card>
+                        </v-card>
+                    </v-col>
+                    <v-responsive
+                            v-if="n === 2"
+                            :key="`width-${n}`"
+                            width="100%"
+                    ></v-responsive>
+                </template>
+            </v-row>
+        </v-container>
         <v-container>
+                <v-row>
                 <v-btn block class="primary">第一个按钮</v-btn>
                <v-card outlined class="text-xs-center ma-3"
                     outlined color="green "
                >
-                <v-card-title>标题</v-card-title>
+                <v-card-title>语文</v-card-title>
 <!--                <v-card-title>标题</v-card-title>-->
                 <v-card-text>
-                    <div class="subheading">语文</div>
+                    <div class="subheading">点击查看答案</div>
                 </v-card-text>
                 <v-card-actions>
                     <v-btn outlined color="green ">
@@ -19,6 +54,7 @@
                     </v-btn>
                 </v-card-actions>
             </v-card>
+                </v-row>
             <v-card outlined class="text-xs-center ma-3">
                 <v-card-title>标题</v-card-title>
                 <v-card-text>
@@ -31,20 +67,8 @@
                         <span>发送消息</span>
                     </v-btn>
                 </v-card-actions>
-<!--                <v-col cols="12" md="6" sm="6">-->
-<!--                    <v-btn block class="primary">第一个按钮</v-btn>-->
-<!--                </v-col>-->
-<!--                <v-col cols="12" md="2" sm="4" >-->
-<!--                    <v-btn block class="primary">第二个按钮</v-btn>-->
-<!--                </v-col>-->
-<!--                <v-col cols="12" md="2" sm="4">-->
-<!--                    <v-btn block class="primary">第三个按钮</v-btn>-->
-<!--                </v-col>-->
-<!--                <v-col cols="12" md="2" sm="4">-->
-<!--                    <v-btn block class="primary">第四个按钮</v-btn>-->
-<!--                </v-col>-->
-            </v-card>
 
+            </v-card>
         </v-container>
 
     </div>
