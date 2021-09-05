a0e39c95e5381ebba23eb63acb0be52681594e43
ckqqqq <1047969038@qq.com>
2021-09-05 16:23:27 +0800

    Update SchoolPrediction.vue

--- src/views/Menu/SchoolPrediction.vue
+++ src/views/Menu/SchoolPrediction.vue
@@ -2,5 +2,20 @@
     <div class="about">
         <h1>This is an schoolprediction page 23333</h1>
         <h1>学校预测</h1>
+        <v-btn @click="ExternalLink(link)">
+        </v-btn>
     </div>
 </template>
+<script>
+    export default {
+        data: () => ({
+            link:'http://www.tju.edu.cn/',
+
+        }),
+        methods :{
+            ExternalLink(link){
+                window.location.href = link;
+            }
+        }
+    }
+</script>
