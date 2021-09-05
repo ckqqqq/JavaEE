26f973a78fd1887c562193367e0ef2b3f4759901
ckqqqq <1047969038@qq.com>
2021-09-05 15:30:51 +0800

    估分

---
+++ src/components/SubjectCard.vue
@@ -0,0 +1,37 @@
+<!--labels 代表表格的横坐标-->
+<!--subjects 代表表格的值-->
+<template>
+  <v-card :color="subject.color" dark>
+    <v-card-text>
+        <v-card-text class="text-center">
+            <div class="display-1 font-weight-black">{{ subject.title }}</div>
+        </v-card-text>
+            <v-row justify="center">
+                <div class="pa-0">北京卷</div>
+
+            </v-row>
+        <v-row justify="center">
+            <v-btn class="d-flex align-center"
+                   color="rgba(0, 0, 0, .1)"
+                   height="50"
+                   width="200"
+                   style="alignment: center"
+            >立即估分</v-btn>
+        </v-row>
+    </v-card-text>
+
+
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'SubjectCard',
+  props: {
+    subject: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
--- src/views/Menu/ScoreEstimation.vue
+++ src/views/Menu/ScoreEstimation.vue
@@ -2,7 +2,25 @@
     <div class="about">
         <h1>This is an ScoreEstimate page 23333</h1>
         <h1>估分</h1>
+
         <v-container class="grey lighten-5">
+            <v-col>
+                <v-select
+                        :items="['北京卷', '天津卷', '账户']"
+
+                        label="试卷"
+                ></v-select>
+            </v-col>
+            <v-col>
+                <v-text-field
+                        label="分数"
+                ></v-text-field>
+            </v-col>
+            <v-row>
+                <v-col v-for="subject in subjects" :key="`${subject.title}`" cols="12" md="4">
+                    <SubjectCard :subject="subject" />
+                </v-col>
+            </v-row>
             <v-row no-gutters>
                 <template v-for="n in 4">
                     <v-col :key="n">
@@ -90,3 +108,26 @@
 
     </div>
 </template>
+<script>
+    import SubjectCard from '@/components/SubjectCard'
+    import SubjectData from '@/data/SubjectData.json'
+    export default {
+        name: 'DashboardPage',
+
+        components: {
+            SubjectCard,
+        },
+        data() {
+            return {
+                subjects: SubjectData,
+                snackbar: false,
+                PaperType:["北京卷","天津卷"]
+            }
+        },
+        methods: {
+            setEmployee(event) {
+                this.snackbar = true
+            }
+        }
+    }
+</script>
